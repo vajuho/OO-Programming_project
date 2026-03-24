3ac1a6597543254b558557407e284ec652bba7df
Juho Varila <varila.juho@gmail.com>
2026-03-24 16:55:31 +0200

    MainActivity xml

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/CityForCompareActivity.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/CityForCompareActivity.java
@@ -15,7 +15,7 @@ public class CityForCompareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_city_for_compare);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.SearchButton), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/oo_ohjelmointi_projekti/CompareInfoActivity.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/CompareInfoActivity.java
@@ -15,7 +15,7 @@ public class CompareInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_compare_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.SearchButton), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
