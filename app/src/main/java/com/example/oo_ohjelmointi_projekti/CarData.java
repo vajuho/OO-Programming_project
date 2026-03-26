28d36aef3a469042cfbd578c56ecfb3c8ec16f39
joelt <joel.tuliluoto@student.lut.fi>
2026-03-26 10:55:37 +0200

    cardata

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/CarData.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/CarData.java
@@ -1,4 +1,27 @@
 package com.example.oo_ohjelmointi_projekti;
 
 public class CarData {
+    private String carAmount;
+    private int year;
+
+    public CarData(String carAmount, int year){
+            this.carAmount = carAmount;
+            this.year = year;
+    }
+
+    public String getCarAmount() {
+        return carAmount;
+    }
+
+    public void setCarAmount(String carAmount) {
+        this.carAmount = carAmount;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
