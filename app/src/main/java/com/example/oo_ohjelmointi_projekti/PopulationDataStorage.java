a8ef5a2be873063a74389835dcea2adff7694408
Juho Varila <varila.juho@gmail.com>
2026-03-26 11:04:03 +0200

    PopulationDataStorage added

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/PopulationDataStorage.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/PopulationDataStorage.java
@@ -1,4 +1,36 @@
 package com.example.oo_ohjelmointi_projekti;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class PopulationDataStorage {
+    private static PopulationDataStorage instance;
+    private String municipality;
+    private ArrayList<PopulationData> populationList = new ArrayList<>();
+
+    private PopulationDataStorage() {
+    }
+
+    public static PopulationDataStorage getInstance() {
+        if (instance == null) {
+            instance = new PopulationDataStorage();
+        }
+        return instance;
+    }
+
+    public ArrayList<PopulationData> getPopulationList() {
+        return populationList;
+    }
+
+    public void addPopulationData(PopulationData populationData) {
+        populationList.add(populationData);
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
 }
