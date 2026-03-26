7ee01f5c368cb89a20f51e32c22a09f47b7d6d88
Juho Varila <varila.juho@gmail.com>
2026-03-26 11:40:24 +0200

    MunicipalityData added

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/MunicipalityData.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/MunicipalityData.java
@@ -9,7 +9,35 @@ public class MunicipalityData {
 
     private WikipediaData wikiUrl;
 
+    private EmploymentData employmentData;
+
     private ArrayList<PopulationData> populations;
 
+    public MunicipalityData(WeatherData weather, CarData carData, WikipediaData wikiUrl, EmploymentData employmentData, ArrayList<PopulationData> populations) {
+        this.weather = weather;
+        this.carData = carData;
+        this.wikiUrl = wikiUrl;
+        this.employmentData = employmentData;
+        this.populations = populations;
+    }
 
+    public WeatherData getWeather() {
+        return weather;
+    }
+
+    public CarData getCarData() {
+        return carData;
+    }
+
+    public WikipediaData getWikiUrl() {
+        return wikiUrl;
+    }
+
+    public ArrayList<PopulationData> getPopulations() {
+        return populations;
+    }
+
+    public EmploymentData getEmploymentData() {
+        return employmentData;
+    }
 }
