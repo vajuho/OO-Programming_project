edf10423c1d99705c11988b6445784093bd91071
Juho Varila <varila.juho@gmail.com>
2026-03-28 11:06:25 +0200

    ListInfoActivity class edits

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/ListInfoActivity.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/ListInfoActivity.java
@@ -24,46 +24,5 @@ public class ListInfoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView cityNameText = findViewById(R.id.CityNameText);
-        TextView cityDescriptionText = findViewById(R.id.CityDescriptionText);
-        TextView cityWikiUrlText = findViewById(R.id.CityWikiUrlText);
-        TextView populationAmountText = findViewById(R.id.PopulationAmountText);
-        TextView populationChangeText = findViewById(R.id.PopulationChangeText);
-        TextView employmentRateText = findViewById(R.id.EmploymentRateText);
-        TextView employmentSelfSufficiencyText = findViewById(R.id.EmploymentSelfSufficiencyText);
-        TextView temperatureText = findViewById(R.id.TemperatureText);
-        TextView weatherDescriptionText = findViewById(R.id.WeatherDescriptionText);
-        TextView carAmountText = findViewById(R.id.CarAmountText);
-        ImageView carImageView = findViewById(R.id.CarImageView);
-
-        MunicipalityData municipalityData = MunicipalityData.getInstance();
-        PopulationDataStorage populationDataStorage = PopulationDataStorage.getInstance();
-
-        cityNameText.setText(populationDataStorage.getMunicipality());
-        cityDescriptionText.setText(municipalityData.getWikiData().getWikiUrlAndDescription().get(0));
-        cityWikiUrlText.setText(municipalityData.getWikiData().getWikiUrlAndDescription().get(1));
-        populationAmountText.setText(String.valueOf(municipalityData.getPopulations().get(municipalityData.getPopulations().size() - 1).getAmount()));
-        populationChangeText.setText(String.valueOf(municipalityData.getPopulations().get(municipalityData.getPopulations().size() - 1).getPopulationIncrease()));
-        employmentRateText.setText(municipalityData.getEmploymentData().getEmploymentRate());
-        employmentSelfSufficiencyText.setText(municipalityData.getEmploymentData().getEmploymentSelfSufficiency());
-        temperatureText.setText(municipalityData.getWeather().getTemperature());
-        weatherDescriptionText.setText(municipalityData.getWeather().getDescription());
-        carAmountText.setText(municipalityData.getCarData().getCarAmount());
-
-        int carAmount = Integer.parseInt(municipalityData.getCarData().getCarAmount());
-
-        int tierOne = 2000;
-        int tierTwo = 10000;
-        int tierThree = 50000;
-
-        if (carAmount < tierOne) {
-            carImageView.setImageResource(R.drawable.car_tier_1);
-        } else if (tierOne <= carAmount && carAmount < tierTwo) {
-            carImageView.setImageResource(R.drawable.car_tier_2);
-        } else if (tierTwo <= carAmount && carAmount < tierThree) {
-            carImageView.setImageResource(R.drawable.car_tier_3);
-        } else {
-            carImageView.setImageResource(R.drawable.car_tier_4);
-        }
     }
 }
