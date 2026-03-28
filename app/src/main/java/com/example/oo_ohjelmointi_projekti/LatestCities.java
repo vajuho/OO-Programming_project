8605520e2b98dedecc8f7dc09116fb38005c8780
Juho Varila <varila.juho@gmail.com>
2026-03-28 15:44:48 +0200

    Multiple changes

---
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/LatestCities.java
@@ -0,0 +1,25 @@
+package com.example.oo_ohjelmointi_projekti;
+
+import java.util.ArrayList;
+
+public class LatestCities {
+    private ArrayList<String> cities = new ArrayList<>();
+    private static LatestCities instance;
+    private LatestCities() {}
+    public static LatestCities getInstance() {
+        if (instance == null) {
+            instance = new LatestCities();
+        }
+        return instance;
+    }
+    public ArrayList<String> getCities() {
+        return cities;
+    }
+    public void addCity (String name) {
+        cities.remove(name);
+        cities.add(0, name);
+        if (cities.size() > 4) {
+            cities.remove(4);
+        }
+    }
+}
