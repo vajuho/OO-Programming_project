5bfc86ecd687040dc23343cdaea1d78b96c01079
joelt <joel.tuliluoto@student.lut.fi>
2026-03-29 13:25:39 +0300

    CityInfoFragment

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/fragments/CityInfoFragment.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/fragments/CityInfoFragment.java
@@ -15,6 +15,15 @@ import com.example.oo_ohjelmointi_projekti.PopulationData;
 import com.example.oo_ohjelmointi_projekti.PopulationDataStorage;
 import com.example.oo_ohjelmointi_projekti.R;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.charts.Pie;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.ArrayList;
 
 public class CityInfoFragment extends Fragment {
