54db03acbedc5a172b63af1086709a8985db4d3b
Juho Varila <varila.juho@gmail.com>
2026-03-30 17:17:23 +0300

    Minor changes

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/QuizViewHolder.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/QuizViewHolder.java
@@ -1,7 +1,13 @@
 package com.example.oo_ohjelmointi_projekti;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class QuizViewHolder extends RecyclerView.ViewHolder {
 
+    public QuizViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
