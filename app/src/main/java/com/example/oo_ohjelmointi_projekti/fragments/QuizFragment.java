df0a7b95ab2e01006be155c5d772e82baac555cc
joelt <joel.tuliluoto@student.lut.fi>
2026-03-31 13:27:29 +0300

    Quizfragment

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/fragments/QuizFragment.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/fragments/QuizFragment.java
@@ -96,7 +96,7 @@ public class QuizFragment extends Fragment {
             int employmentRate = (int) Double.parseDouble(yearPlusEmployment.split(":")[1]);
             String yearPlusSufficiency = municipalityData.getEmploymentData().getEmploymentSelfSufficiency();
             int employmentSelfSufficiency = (int) Double.parseDouble(yearPlusSufficiency.split(":")[1]);
-            double carsPerPopulation = ((double) carAmount / populationLatest) * 100;
+            double carsPerPopulation = Math.round(((((double) carAmount / populationLatest) * 100) * 10.0) / 10.0);
             int correctAnswerIndex = 0;
 
             ArrayList<String> weatherDescriptionsList = new ArrayList<>(Arrays.asList(
