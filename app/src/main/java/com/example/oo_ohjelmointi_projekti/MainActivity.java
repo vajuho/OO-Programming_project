3e3b9b2576b78819f09aaca9faca2251bc7bd127
joelt <joel.tuliluoto@student.lut.fi>
2026-03-31 16:35:51 +0300

    Loading Gif

--- app/src/main/java/com/example/oo_ohjelmointi_projekti/MainActivity.java
+++ app/src/main/java/com/example/oo_ohjelmointi_projekti/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         if (employmentRate == null) {
                             StatusText.setText("Haku epäonnistui, kaupunkia ei ole olemassa tai se on kirjoitettu väärin.");
+                            Glide.with(MainActivity.this).asGif().load(R.drawable.fail).into(StatusImage);
                             return;
                         }
                         PopulationDataStorage populationStorage = PopulationDataStorage.getInstance();
